fbce302f01b8a5888a4df1779794d6b390c0c604
Olivier <olivierkotwica@gmail.com>
2023-06-22 11:19:41 +0200

    modif main

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ st.title("US Sign Language Vision")
 
 uploaded_file = st.file_uploader("Enter an image :", type=["jpg", "jpeg", "png"])
 
-model = YOLO('best_model.pt')
+model = YOLO('best.pt')
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     resized_image = image.resize((800, 800))
     results = model.predict(resized_image)
     st.image(results[0].plot())
+
